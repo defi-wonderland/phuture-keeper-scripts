49efcee132e02bd8b6344d4a68cafe50f82dd301
Eugene Lepeico <eugenelepeico@gmail.com>
2022-10-20 13:52:36 +0300

    Update config.ts

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from '@dethcrypto/eth-sdk';
 export default defineConfig({
   contracts: {
     mainnet: {
-      orderJob: '0x133A4273589c2eE5F9Fe28898B68aC1B4B1BA9B0',
+      orderJob: '0x656027367B5e27dC21984B546e64dC24dBFaA187',
       depositManagerJob: '0xa61d82a9127B1c1a34Ce03879A068Af5b786C835',
     },
   },
