9d2fa5882801aa5c933c34917e8ba26a63ef238a
0xGorilla <0xGorilla@protonmail.com>
2023-02-18 15:00:26 +0100

    feat: changing wss to https

--- src/shared/setup.ts
+++ src/shared/setup.ts
@@ -6,7 +6,7 @@ import type { InitialSetup } from '../utils/types';
 dotenv.config();
 
 export function loadInitialSetup(): InitialSetup {
-  const provider = new providers.WebSocketProvider(getEnvVariable('RPC_WSS_URI'));
+  const provider = new providers.JsonRpcProvider(getEnvVariable('RPC_HTTPS_URI'));
   const txSigner = new Wallet(getEnvVariable('TX_SIGNER_PRIVATE_KEY'), provider);
   const bundleSigner = new Wallet(getEnvVariable('BUNDLE_SIGNER_PRIVATE_KEY'), provider);
   return {
--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -3,7 +3,7 @@ import type { providers, Wallet } from 'ethers';
 export * from './ordering';
 
 export type InitialSetup = {
-  provider: providers.WebSocketProvider;
+  provider: providers.WebSocketProvider | providers.JsonRpcProvider;
   txSigner: Wallet;
   bundleSigner: Wallet;
 };
