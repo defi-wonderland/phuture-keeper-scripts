a731fcc20748c758fc113ea6748ff38cabe3dc76
0xGorilla <0xGorilla@defi.sucks>
2023-03-27 11:04:30 +0200

    fix: renamed import

--- src/helpers/propagate/index.ts
+++ src/helpers/propagate/index.ts
@@ -1,5 +1,5 @@
 export { getPropagateParams as getPropagateParamsArbitrum } from "./arbitrum";
 export { getPropagateParams as getPropagateParamsBnb } from "./bnb";
 export { getPropagateParams as getPropagateParamsGnosis } from "./gnosis";
-export { getPropagateParams as getPropagateParamsZkSync } from "./zkSync";
+export { getPropagateParams as getPropagateParamsZkSync } from "./zksync";
 export { getPropagateParams as getPropagateParamsConsensys } from "./consensys";
