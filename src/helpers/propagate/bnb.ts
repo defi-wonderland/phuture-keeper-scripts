f9dc80cf90f1fc3a3a4546ec11415a35644e02c5
Layne Haber <layne.haber@gmail.com>
2023-07-11 17:38:14 -0600

    feat: generate proper arguments for wormhole propagation

--- src/helpers/propagate/bnb.ts
+++ src/helpers/propagate/bnb.ts
@@ -1,18 +1,16 @@
-import {Contract} from 'ethers';
-import {getMainnetSdk} from '@dethcrypto/eth-sdk-client';
+import { utils } from 'ethers';
+import { getMainnetSdk } from '@dethcrypto/eth-sdk-client';
 
-import {type ExtraPropagateParameters, type InitialSetup} from 'src/utils/types';
+import { type ExtraPropagateParameters, type InitialSetup } from 'src/utils/types';
 
-export const getPropagateParameters = async ({txSigner, provider}: InitialSetup): Promise<ExtraPropagateParameters> => {
+export const getPropagateParameters = async ({ txSigner, provider }: InitialSetup): Promise<ExtraPropagateParameters> => {
   const mainnetSdk = getMainnetSdk(txSigner);
   const ambAddress = await mainnetSdk.bnbHubConnector.AMB();
 
-  const ambContract = new Contract(
-    ambAddress,
-    ['function calcSrcFees(string calldata _appID, uint256 _toChainID, uint256 _dataLength) external view returns (uint256)'],
-    provider,
-  );
-  const fee = await ambContract.calcSrcFees('', 56, 32);
+  // see: https://github.com/connext/monorepo/blob/e28db100e55d28d8a9d509959d603f9ced58de25/packages/agents/lighthouse/src/tasks/propagate/helpers/bnb.ts#L54-L57
+  const gasLimit = "200000";
+  const fee = await mainnetSdk.bnbHubConnector.quoteEVMDeliveryPrice(gasLimit, ambAddress);
+  const encodedData = utils.defaultAbiCoder.encode(["uint256"], [gasLimit]);
 
-  return {connector: '', fee, encodedData: '0x'};
+  return { connector: '', fee: fee.toString(), encodedData };
 };
